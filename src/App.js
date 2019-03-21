4acf50b90b78c5d1fb8a371697efbccabef027ec
Sofía Santos Melián <sofiasantosmelian@gmail.com>
2019-03-21 13:53:49 +0100

    final api url still error

--- src/App.js
+++ src/App.js
@@ -23,16 +23,25 @@ class App extends React.Component {
 
 		const api_call = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric');
 		const data = await api_call.json();
-		console.log(data);
-
-		this.setState({
-			temperature: data.main.temp,
-			city: data.name,
-			country: data.sys.country,
-			humidity: data.main.humidity,
-			description: data.weather[0].description,
-			error:undefined
-		});
+		if (city && country ){
+			this.setState({
+				temperature: data.main.temp,
+				city: data.name,
+				country: data.sys.country,
+				humidity: data.main.humidity,
+				description: data.weather[0].description,
+				error:""
+			});
+		} else {
+			this.setState({
+				temperature: undefined,
+				city: undefined,
+				country:undefined,
+				humidity: undefined,
+				description: undefined,
+				error:"please, enter the value"
+			});
+		}
 	}
 
 	render() {
