16481514b9242ee6e998d75004e873f116285b02
Sofía Santos Melián <sofiasantosmelian@gmail.com>
2019-03-22 10:26:42 +0100

    final

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
-const API_KEY = 'e662da856d7a4c6258b421bc82fbc9fc';
-
 class App extends React.Component {
 	state = {
 		temperature: undefined,
@@ -21,7 +19,7 @@ class App extends React.Component {
 		const city = e.target.elements.city.value;
 		const country = e.target.elements.country.value;
 
-		const api_call = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric');
+		const api_call = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Madrid,ES&appid=e662da856d7a4c6258b421bc82fbc9fc&units=metric');
 		const data = await api_call.json();
 		if (city && country ){
 			this.setState({
@@ -47,17 +45,32 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Titles />
-				<Form getWeather={this.getWeather}/>
-				<Weather temperature={this.state.temperature}
-							city={this.state.city}
-							country={this.state.country}
-							humidity={this.state.humidity}
-							description={this.state.description}
-							error={this.state.error}/>
+				<div className="wrapper">
+					<div className="main">
+						<div className="container">
+							<div className="row">
+								<div className="col-xs-5 title-container">	
+									<Titles />
+								</div>
+								<div className="col-xs-7 form-container">
+									<Form getWeather={this.getWeather}/>
+									<Weather 
+										temperature={this.state.temperature}
+										city={this.state.city}
+										country={this.state.country}
+										humidity={this.state.humidity}
+										description={this.state.description}
+										error={this.state.error}/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
 };
+
+	
 
 export default App;
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Titles = () => {
+const Titles = () => (
     <div>
-        <h1>Weather Finder</h1>
-        <p>FInd out temperature, conditions and more...</p>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <h3 className="title-container__subtitle">FInd out temperature, conditions and more...</h3>
     </div>
-}
-
+);
+ 
 export default Titles;
